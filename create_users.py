2086d000968b7ba3df3a6314c9b51e16ddcc9500
Pablocst <pablocstredes@gmail.com>
2023-10-05 17:30:09 -0300

    JS-1 Create users from a file using REST API

---
+++ create_users.py
@@ -0,0 +1,35 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://palbuquerque.atlassian.net/rest/api/3/user"
+
+auth = HTTPBasicAuth("palbuquerque@atlassian.com", "ATATT3xFfGF0FLxYt4pgiI0CbDD7hvEUF-97-Bke-77B4_MIvzGDE5HupphMdM-UcesrCFnnJXsB7BFd0gfcpjxCRc5Frc7pej8C2A6WtMQ6Lh4SdzyAiYh1M8oqDZgdGmlkswCXqkEPKIynMPsNBZFNjg_ceHQTD5J05Stlpu3PwwJ2F2Q5Im8=9FAC86E9")
+
+with open(sys.argv[1], 'r') as f:
+    csvfile = f.readlines()
+    
+for line in csvfile:
+     user_mail_address = line.strip().split(",")[1]
+    
+     headers = {
+     "Accept": "application/json",
+     "Content-Type": "application/json"
+     }
+     payload = json.dumps( {
+     "emailAddress": f"{user_mail_address}"
+     } )
+     response = requests.request(
+     "POST",
+     url,
+     data=payload,
+     headers=headers,
+     auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+
